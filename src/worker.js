33b031bb8d3903d54589d376b4da3b74f4963422
sirpy <hadar@gooddollar.org>
2024-01-03 14:54:53 +0200

    add: png optimize

--- src/worker.js
+++ src/worker.js
@@ -1,77 +1,117 @@
-import { Web3Storage } from 'web3.storage'
-import { CID } from 'multiformats/cid'
-import { NFTStorage } from 'nft.storage'
+import { Web3Storage } from "web3.storage";
+import { CID } from "multiformats/cid";
+import { NFTStorage } from "nft.storage";
+import upng from "upng-js";
 
-let env
+let env;
+
+const optimizeImage = async (file) => {
+  try {
+    const decoded = upng.decode(await file.arrayBuffer());
+    const frames = upng.toRGBA8(decoded);
+    const optimized = Buffer.from(
+      upng.encode(frames, decoded.width, decoded.height, 256)
+    );
+    return new Blob([optimized]);
+  } catch (e) {
+    console.error("not png?", e.message);
+    return file;
+  }
+};
 
 async function handleRequest(request) {
   const storage = new Web3Storage({
-    token: env.WEB3STORAGE_TOKEN
-  })
+    token: env.WEB3STORAGE_TOKEN,
+  });
 
-  const { corsHeaders } = getCorsHeaders(request)
+  const { corsHeaders } = getCorsHeaders(request);
 
   // Parse the request to FormData
-  const formData = await request.formData()
+  const formData = await request.formData();
   // Get the File from the form. Key for the file is 'image' for me
-  const file = formData.get('file')
-  const cid = await storage.put([file], { wrapWithDirectory: false, maxRetries: 3 }).catch(e => console.log(e))
+  const file = formData.get("file");
+  const optimized = await optimizeImage(file);
+  const cid = await storage
+    .put([file], { wrapWithDirectory: false, maxRetries: 3 })
+    .catch((e) => console.log(e));
 
-  return new Response(JSON.stringify({ cid }), { headers: { ...corsHeaders, 'Content-Type': 'application/json' } })
+  return new Response(JSON.stringify({ cid }), {
+    headers: { ...corsHeaders, "Content-Type": "application/json" },
+  });
 }
 
 async function handleRequestNft(request) {
   const nftstorage = new NFTStorage({
-    token: env.NFTSTORAGE_TOKEN
-  })
-  const { corsHeaders } = getCorsHeaders(request)
+    token: env.NFTSTORAGE_TOKEN,
+  });
+  const { corsHeaders } = getCorsHeaders(request);
 
   // Parse the request to FormData
-  const formData = await request.formData()
+  const formData = await request.formData();
   // Get the File from the form. Key for the file is 'image' for me
-  const file = formData.get('file')
-  const cid = await nftstorage.storeBlob(file, { wrapWithDirectory: false, maxRetries: 3 }).catch(e => console.log(e))
-  return new Response(JSON.stringify({ cid }), { headers: { ...corsHeaders, 'Content-Type': 'application/json' } })
+  const file = formData.get("file");
+  const optimized = await optimizeImage(file);
+  const cid = await nftstorage
+    .storeBlob(optimized, { wrapWithDirectory: false, maxRetries: 3 })
+    .catch((e) => console.log(e));
+  return new Response(JSON.stringify({ cid }), {
+    headers: { ...corsHeaders, "Content-Type": "application/json" },
+  });
 }
 
-const getCorsHeaders = request => {
-  const origin = request.headers.get('Origin') || ''
-  const isAllowed = origin.match(/gooddollar\.org$|good.*\.netlify\.app$|goodceramic\-.*\.herokuapp\.com$|\/\/localhost/)
+const getCorsHeaders = (request) => {
+  const origin = request.headers.get("Origin") || "";
+  const isAllowed = origin.match(
+    /gooddollar\.org$|good.*\.netlify\.app$|goodceramic\-.*\.herokuapp\.com$|\/\/localhost/
+  );
 
   const corsHeaders = {
-    'Access-Control-Allow-Origin': isAllowed ? origin : '',
-    'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    'Access-Control-Allow-Credentials': 'true'
-  }
+    "Access-Control-Allow-Origin": isAllowed ? origin : "",
+    "Access-Control-Allow-Methods": "GET, HEAD, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Credentials": "true",
+  };
 
-  return { isAllowed, corsHeaders }
-}
+  return { isAllowed, corsHeaders };
+};
 
 async function handleOptions(request) {
-  const { corsHeaders } = getCorsHeaders(request)
+  const { corsHeaders } = getCorsHeaders(request);
 
   return new Response(null, {
-    headers: corsHeaders
-  })
+    headers: corsHeaders,
+  });
 }
 
 export default {
   async fetch(request, myenv, context) {
-    env = myenv //store env in globals so its available in methods
+    env = myenv; //store env in globals so its available in methods
     switch (request.method) {
-      case 'OPTIONS':
-        return handleOptions(request)
-      case 'POST': {
-        const { isAllowed, corsHeaders } = getCorsHeaders(request)
+      case "OPTIONS":
+        return handleOptions(request);
+      case "POST": {
+        const { isAllowed, corsHeaders } = getCorsHeaders(request);
         if (!isAllowed) {
-          return new Response(JSON.stringify({ error: 'origin not allowed' }), { status: 400 })
+          return new Response(JSON.stringify({ error: "origin not allowed" }), {
+            status: 400,
+          });
         }
-        if (env.USE_WEB3STORAGE) return handleRequest(request)
-        return handleRequestNft(request)
+        let response;
+        try {
+          if (env.USE_WEB3STORAGE) response = await handleRequest(request);
+          else response = await handleRequestNft(request);
+          return response;
+        } catch (e) {
+          console.error("ipfsgateway request failed", e.message, e);
+          return new Response(JSON.stringify({ error: e.message }), {
+            status: 400,
+          });
+        }
       }
       default:
-        return new Response(JSON.stringify({ error: 'method not allowed' }), { status: 400 })
+        return new Response(JSON.stringify({ error: "method not allowed" }), {
+          status: 400,
+        });
     }
-  }
-}
+  },
+};
